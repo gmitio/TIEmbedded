54ba2e502837b288525ae36b87821796d25ebd7c
Graison Mitio <graison.mitio@gmail.com>
2020-01-16 00:46:11 -0600

    Update I2CTest.c
    Finished untested version of I2C initialization function and clarified a couple of comments

--- I2CTest/I2CTest.c
+++ I2CTest/I2CTest.c
@@ -7,13 +7,13 @@
 
 #include "tm4c123gh6pm.h"
 
-void i2c_init()
+void i2c_init(void) // Function to initialize transmission to the sensor
 {
     SYSCTL_RCGCI2C_R |= 0x01;   // Enable I2C clock in RCGCI2C
     SYSCTL_RCGCGPIO_R |= 0x02;  // Enable GPIO clocking on Port B for I2C0
     GPIO_PORTB_AFSEL_R |= 0x0C; // Set port AFSEL to I2C
-    // Enable I2CSDA with open-drain operation (datasheet pg. 676)
-    // Initialize I2C master (I2CMCR)
+    GPIO_PORTB_ODR_R |= 0x08;   // Enable open-drain operation on SDA (datasheet pg. 676)
+    I2C0_MCR_R |= 0x10; // Initialize I2C as master (I2CMCR)
     I2C0_MTPR_R = 7;    // Set desired clock speed (I2CMTPR), 100kbps SCL on 16MHz system clock
                               //TPR = (System Clock / (2*SCL_LP + SCL_HP) * SCL_CLK) - 1
     I2C0_MSA_R = 0x90;  // Specify slave address (1001000) and next operation (I2CMSA)
